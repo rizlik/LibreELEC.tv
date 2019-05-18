60a538089e3fd577da76e02e9b1ec4b56b1c786b
chewitt <github@chrishewitt.net>
2019-05-18 06:22:39 +0000

    dtc: add fdtdump to build list

--- packages/tools/dtc/package.mk
+++ packages/tools/dtc/package.mk
@@ -11,13 +11,14 @@ PKG_URL="https://git.kernel.org/pub/scm/utils/dtc/dtc.git/snapshot/$PKG_VERSION.
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="The Device Tree Compiler"
 
-PKG_MAKE_OPTS_TARGET="dtc fdtput fdtget libfdt"
+PKG_MAKE_OPTS_TARGET="dtc fdtdump fdtget fdtput libfdt"
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/bin
     cp -P $PKG_BUILD/dtc $INSTALL/usr/bin
-    cp -P $PKG_BUILD/fdtput $INSTALL/usr/bin/
+    cp -P $PKG_BUILD/fdtdump $INSTALL/usr/bin/
     cp -P $PKG_BUILD/fdtget $INSTALL/usr/bin/
+    cp -P $PKG_BUILD/fdtput $INSTALL/usr/bin/
   mkdir -p $INSTALL/usr/lib
     cp -P $PKG_BUILD/libfdt/libfdt.so $INSTALL/usr/lib/
 }
